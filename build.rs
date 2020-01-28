10194f40e4cd818290af3f6e0e48f60f12ce3dde
Michael Malyshev <michaelm@altia.com>
2020-01-28 11:25:24 +0300

    Fix build on windows platforms
    This fix addresses https://github.com/houqp/leptess/issues/3
    
    This library on Windows is destrebuted using VCPKG
    (https://github.com/microsoft/vcpkg). This commit uses crate 'vcpgk' to
    locate installed libraries and automatically provide library name for
    cargo.
    
    bindgen needs extra include directory to locate library headers. It
    is implemented using clang_arg() method.
    
    VCPKG is also available on Linux and MacOS but the most of users use
    standard package manages so vcpkg is called on Windows only

--- build.rs
+++ build.rs
@@ -2,16 +2,36 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+#[cfg(windows)]
+use vcpkg;
+
+#[cfg(windows)]
+fn find_tesseract_system_lib() -> Option<String> {
+    let lib = vcpkg::Config::new().find_package("leptonica").unwrap();
+
+    let include = lib
+        .include_paths
+        .iter()
+        .map(|x| x.to_string_lossy())
+        .collect::<String>();
+    Some(include)
+}
+
+#[cfg(not(windows))]
+fn find_tesseract_system_lib() -> Option<String> {
+    println!("cargo:rustc-link-lib=tesseract");
+    None
+}
 
 fn main() {
     // Tell cargo to tell rustc to link the system tesseract
     // and leptonica shared libraries.
-    println!("cargo:rustc-link-lib=tesseract");
+    let clang_extra_include = find_tesseract_system_lib();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
-    let capi_bindings = bindgen::Builder::default()
+    let mut capi_bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
         .header("wrapper_capi.h")
@@ -22,16 +42,28 @@ fn main() {
         .blacklist_type("_IO_FILE")
         .blacklist_type("_IO_codecvt")
         .blacklist_type("_IO_marker")
-        .blacklist_type("_IO_wide_data")
-        // Finish the builder and generate the bindings.
+        .blacklist_type("_IO_wide_data");
+
+    if let Some(clang_extra_include) = &clang_extra_include {
+        capi_bindings = capi_bindings.clang_arg(format!("-I{}", clang_extra_include));
+    }
+    // Finish the builder and generate the bindings.
+    let capi_bindings = capi_bindings
         .generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate capi bindings");
 
-    let public_types_bindings = bindgen::Builder::default()
+    let mut public_types_bindings = bindgen::Builder::default()
         .header("wrapper_public_types.hpp")
         .whitelist_var("^k.*")
-        .blacklist_item("kPolyBlockNames")
+        .blacklist_item("kPolyBlockNames");
+
+    if let Some(clang_extra_include) = &clang_extra_include {
+        public_types_bindings =
+            public_types_bindings.clang_arg(format!("-I{}", clang_extra_include));
+    }
+    
+    let public_types_bindings = public_types_bindings
         .generate()
         .expect("Unable to generate public types bindings");
 
