5e74171f51ff7aab26fdd1eb6557ab1c7f12354c
Chris Couzens <ccouzens@gmail.com>
2022-02-06 22:06:05 +0000

    On Mac, hardcode the public type bindings  (#17)
    * Reorder build.rs slo public types are in own function
    
    This way I'll be able use a different version of the function on Mac.
    
    * On Mac, hardcode the public type bindings
    
    Do not generate them, as the Mac clang is a source of regular issues
    building them.
    
    They haven't changed in 5 years since they were introduced, so I think
    this is a fairly safe change
    
    https://github.com/tesseract-ocr/tesseract/blob/main/include/tesseract/publictypes.h#L30-L43
    (check the blame).
    
    Closes https://github.com/ccouzens/tesseract-sys/issues/16

---
+++ src/public_types_bindings_mac.rs
@@ -0,0 +1,4 @@
+pub const kPointsPerInch: ::std::os::raw::c_int = 72;
+pub const kMinCredibleResolution: ::std::os::raw::c_int = 70;
+pub const kMaxCredibleResolution: ::std::os::raw::c_int = 2400;
+pub const kResolutionEstimationFactor: ::std::os::raw::c_int = 10;
