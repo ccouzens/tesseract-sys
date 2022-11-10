b0c10c620baf9679d4eab95b8dceded1ca4d78d9
Chris Couzens <ccouzens@gmail.com>
2022-11-10 23:31:00 +0000

    Free the string data in the tests
    There were only 2 tests, so any leak would be quickly dealt with by the
    process finishing. But it wasn't a good look to have a leak.
    
    I've also switched to some c-string handling I've learned from recent
    c-integration work.

--- src/lib.rs
+++ src/lib.rs
@@ -13,24 +13,21 @@ mod tests {
     use leptonica_sys::{pixFreeData, pixRead};
     use std::ffi::CStr;
     use std::ptr;
-    use std::str;
 
     #[test]
     fn ocr() {
         unsafe {
             let cube = TessBaseAPICreate();
-            TessBaseAPIInit3(
-                cube,
-                ptr::null(),
-                CStr::from_bytes_with_nul_unchecked(b"eng\0").as_ptr(),
-            );
-            let image = pixRead(CStr::from_bytes_with_nul_unchecked(b"img.png\0").as_ptr());
+            TessBaseAPIInit3(cube, ptr::null(), b"eng\0".as_ptr().cast());
+            let image = pixRead(b"img.png\0".as_ptr().cast());
             TessBaseAPISetImage2(cube, image);
             TessBaseAPIRecognize(cube, ptr::null_mut());
+            let text = TessBaseAPIGetUTF8Text(cube);
             assert_eq!(
-                str::from_utf8(CStr::from_ptr(TessBaseAPIGetUTF8Text(cube)).to_bytes()),
+                CStr::from_ptr(text).to_str(),
                 Ok(include_str!("../img.txt"))
             );
+            TessDeleteText(text);
             pixFreeData(image);
             TessBaseAPIDelete(cube);
         }
